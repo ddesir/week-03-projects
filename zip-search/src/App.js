8e1daaa0935f0db8ea9382af20b7403c77489e7e
Deon Desir <d.desir@gmail.com>
2019-10-11 09:46:18 -0400

    Pre Catalina upgrade

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -1,10 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'; // Default import and named import
 import logo from './logo.svg';
 import './App.css';
 
 
 function City(props) {
-  return (<div></div>);
+  return (
+  	<div>
+  		<p>{ props.city }, { props.state }</p>
+  		<ul>
+  	</div>
+  );
 }
 
 function ZipSearchField(props) {
@@ -12,24 +17,56 @@ function ZipSearchField(props) {
   	<form>
   		<label>
   			Zip Code: 
-  			<input type='text' name='zipcode' required />
+  			<input type='text' name='zipcode' onChange={ props.onChange } required />
   		</label>
   	</form>);
 }
 
 
 class App extends Component {
+	state = {
+		cities: [],
+		errorMessage: '',
+		zipCode: ''
+	};
+	
+	onZipChange = async event => {
+		const zipCode = event.target.value;
+		this.setState({ zipCode: zipCode });
+		
+		if (zipCode.length === 5) {
+			const response = await fetch (
+				`http://ctp-zip-api.herokuapp.com/zip/${zipCode}`
+			);
+			
+			if (response.status === 404) {
+				this.setState({ errorMessage: 'Not Found!' });
+			}
+			
+			const cities = await response.json();
+			this.setState({ cities: cities });
+		}
+	};
+	
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-        <ZipSearchField />
+        <ZipSearchField onChange = { this.onZipChange } />
         <div>
-          <City />
-          <City />
+        	{this.state.cities.map(city => {
+        		return (
+        			<City
+        				key = { city.RecordNumber }
+        				city = { city.City }
+        				state = { city.State }
+        			/>
+        		);
+        	})}
         </div>
+        { this.state.errorMessage }
       </div>
     );
   }
